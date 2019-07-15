9338cb7fef655d1267c66b9fed972b787ddde4eb
JT <certainthings@gmail.com>
2019-07-15 14:32:01 +1000

    ruby conditional exercises completed

--- 05-ruby/conditionals/suburbs.rb
+++ 05-ruby/conditionals/suburbs.rb
@@ -1,3 +1,20 @@
 # ### 3. Sydney Suburbs
 # - Create a program that asks what suburbs you live in.
 # - Depending on the answer, print an appropriate response of your choosing
+
+print "What suburb do you live in: "
+residence = gets.chomp.capitalize
+
+# Case expression
+puts case residence
+  when 'Glebe'
+    "Beautiful foreshore walk"
+  when 'Mascot'
+    "Cool crumbling apartment block"
+  when 'Fairlight'
+    "What a fancy name"
+  when 'Cabramatta'
+    "Fine dining. Also heroin"
+  else
+    "I'm sure that's a lovely place"
+end
