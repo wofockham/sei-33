b128251049a666096bf608b2e8e891025d4cd0f5
JT <certainthings@gmail.com>
2019-07-17 11:53:12 +1000

    sinatra terrible calculator

--- 05-sinatra/intro.rb
+++ 05-sinatra/intro.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 # Root or home page
 get '/' do
@@ -13,3 +14,37 @@ end
 get '/goodbye' do
   "Goodbye Cruel World!!!!"
 end
+
+get '/lucky_number' do
+  "Here is your lucky number: #{ rand 1..40 }"
+end
+
+get '/uptime' do
+  "The server uptime is: #{ `uptime` }"
+end
+
+# Marx Brothers Fanclub
+
+# An infinite number of URLs
+get '/fanclub/:name'  do
+  "#{ params[:name].capitalize } is a proud member of the Marx Brothers fanclub"
+end
+
+get '/fanclub/:first/:last' do
+  "#{ params[:first].capitalize } #{ params[:last].upcase } is a proud member of the Marx Brothers fanclub"
+end
+
+get '/fanclub/:first/:last/:favourite' do
+  "#{ params[:first].capitalize } #{ params[:last].upcase } is a proud member of the Marx Brothers fanclub and their favourite Marx Brother is #{ params[:favourite].capitalize }"
+end
+
+# Second crappiest calculator of the course
+get '/multiply/:x/:y' do
+  result = params[:x].to_f * params[:y].to_f
+  "The result is <strong>#{ result }"
+end
+
+# Significant problems:
+
+# 1. Where is our HTML? How do write without self harming?
+# 2. How do we get input from a user without them manually changing the URL?
