67277bc481822a9dbbd3a18cc71e4cd52e10d605
JT <certainthings@gmail.com>
2019-07-19 12:14:28 +1000

    ruby classes and inheritance

--- 05-ruby/classes.rb
+++ 05-ruby/classes.rb
@@ -1,4 +1,3 @@
-require 'pry'
 # Objects in Ruby store data and functionality (methods) together.
 
 # Kind of like a hash -- it has key/value pairs to store
@@ -7,16 +6,49 @@ require 'pry'
 # You can't add random keys later like you can with a hash.
 # AND ALSO: unlike a hash, an object has methods.
 
-class MarxBrother
-  attr_accessor :name, :instrument, :vice # Macro
+require 'pry'
+
+# Superclass
+class Actor
+  def award_speech
+    "I would like to thank my parents and my agent. We did it baby."
+  end
+
+  def deny_allegations
+    "I deny that and I was drunk and I don't remember and he's not type."
+  end
 end
 
-groucho = MarxBrother.new
-groucho.name=('Groucho Julius Marx')
-groucho.instrument=('guitar')
-groucho.vice=('cigars')
+# Inheritance
+class Stooge < Actor
+  def terrible?
+    true
+  end
+end
+
+class MarxBrother < Actor
+  attr_accessor :name, :instrument, :vice # Macro
+
+  def initialize(n='', i='', v='mocking authority') # Variadic
+    @name = n
+    @instrument = i
+    @vice = v
+  end
+
+  def accept_award
+  end
+
+  def play
+    "My name is #{ @name } and I play the #{ @instrument }."
+  end
+end
+
+groucho = MarxBrother.new 'Groucho Marx', 'guitar', 'cigars'
+# groucho.name = 'Groucho Julius Marx'
+# groucho.instrument = 'guitar'
+# groucho.vice = 'cigars'
 
 harpo = MarxBrother.new
-harpo.name=('Harpo Marx')
+harpo.name = 'Harpo Marx'
 
 binding.pry
