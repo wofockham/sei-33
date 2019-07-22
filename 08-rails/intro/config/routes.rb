f3c8666c7d3a7314fce1e00368f844173821ff02
JT <certainthings@gmail.com>
2019-07-22 13:01:10 +1000

    rails routing controller and view examples

---
+++ 08-rails/intro/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root :to => 'pages#hello'
+  get '/hello' => 'pages#hello'
+  get '/faq' => 'pages#faq'
+  get '/about' => 'pages#about'
+
+  # Car stuff
+  get '/auto/:color' => 'auto#color'
+  get '/auto/:hp/:torque' => 'auto#engine'
+
+  # Calculator stuff
+  get '/calc/:x/:y/:operator' => 'calc#calculate'
+end
