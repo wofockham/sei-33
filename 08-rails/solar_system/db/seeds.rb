3e259bc7e78965e178395fe79333f03f3fc665bc
JT <certainthings@gmail.com>
2019-07-23 12:59:31 +1000

    solar system crud

---
+++ 08-rails/solar_system/db/seeds.rb
@@ -0,0 +1,5 @@
+Planet.destroy_all # I am Galactus
+
+Planet.create :name => 'Earth', :orbit => 24, :moons => 1
+Planet.create :name => 'Venus', :orbit => 17, :moons => 0
+Planet.create :name => 'Mars', :orbit => 33, :moons => 6
