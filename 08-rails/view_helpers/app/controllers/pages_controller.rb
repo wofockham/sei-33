fc4a36f751e09b8737603fbacb19fc0d25f02906
JT <certainthings@gmail.com>
2019-07-23 10:52:32 +1000

    view helpers part i

---
+++ 08-rails/view_helpers/app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def numbers
+    @amount = 232938.28743
+    @large_number = 239343498
+    @phone = 2125551212
+  end
+
+  def text
+    @numbers = (1..20).to_a.shuffle
+    @friend_count = 2
+    @enemy_count = 11
+    @story = "A very very very very very very very very long time ago in a galaxy far far far far far far far far away."
+  end
+end
