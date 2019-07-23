04542ecc76eff742facf7070435b266978a0df23
JT <certainthings@gmail.com>
2019-07-23 14:29:53 +1000

    planets crud part 2

--- 08-rails/solar_system/app/controllers/planets_controller.rb
+++ 08-rails/solar_system/app/controllers/planets_controller.rb
@@ -22,6 +22,22 @@ class PlanetsController < ApplicationController
     redirect_to planet_path(planet.id)
   end
 
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def update
+    planet = Planet.find params[:id]
+    planet.name = params[:name]
+    planet.image = params[:image]
+    planet.orbit = params[:orbit]
+    planet.diameter = params[:diameter]
+    planet.mass = params[:mass]
+    planet.moons = params[:moons]
+    planet.save
+    redirect_to planet_path(planet.id)
+  end
+
   def destroy
     planet = Planet.find params[:id]
     planet.destroy
--- 08-rails/solar_system/config/routes.rb
+++ 08-rails/solar_system/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/planets/new' => 'planets#new', :as => :new_planet
   post '/planets' => 'planets#create'
   get '/planets/:id' => 'planets#show', :as => :planet
+  get '/planets/:id/edit' => 'planets#edit', :as => :edit_planet
+  post '/planets/:id' => 'planets#update'
   delete '/planets/:id' => 'planets#destroy'
 end
