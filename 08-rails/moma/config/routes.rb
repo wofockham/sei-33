23f421c29539372c5604570d61aea8cc21f0d624
JT <certainthings@gmail.com>
2019-07-24 12:45:52 +1000

    artists index show and new

--- 08-rails/moma/config/routes.rb
+++ 08-rails/moma/config/routes.rb
@@ -1,3 +1,15 @@
+# == Route Map
+#
+#      Prefix Verb   URI Pattern                 Controller#Action
+#     artists GET    /artists(.:format)          artists#index
+#             POST   /artists(.:format)          artists#create
+#  new_artist GET    /artists/new(.:format)      artists#new
+# edit_artist GET    /artists/:id/edit(.:format) artists#edit
+#      artist GET    /artists/:id(.:format)      artists#show
+#             PATCH  /artists/:id(.:format)      artists#update
+#             PUT    /artists/:id(.:format)      artists#update
+#             DELETE /artists/:id(.:format)      artists#destroy
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists
 end
