c29cf99ea46d97b3b9d79cd882839c6109dd9892
JT <certainthings@gmail.com>
2019-07-24 15:39:36 +1000

    works crud

--- 08-rails/moma/db/schema.rb
+++ 08-rails/moma/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_004832) do
+ActiveRecord::Schema.define(version: 2019_07_24_045904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2019_07_24_004832) do
     t.text "nationality"
     t.date "dob"
     t.text "period"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.text "title"
+    t.text "year"
+    t.text "medium"
+    t.text "style"
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
