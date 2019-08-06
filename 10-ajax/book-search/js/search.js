36de04b64c9750f67ba069a8e1bcd375352f3785
JT <certainthings@gmail.com>
2019-08-06 10:57:25 +1000

    AJAX book search solution without jquery

---
+++ 10-ajax/book-search/js/search.js
@@ -0,0 +1,24 @@
+const search = document.getElementById('search');
+
+search.addEventListener('submit', function (event) {
+
+  event.preventDefault(); // Stay here: Prevent the form submission.
+
+  const bookTitle = document.getElementById('book_title').value;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=title:${ bookTitle }`;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.send();
+
+  xhr.onreadystatechange = function () {
+
+    if (xhr.readyState !== 4) return;
+
+    const data = JSON.parse( xhr.responseText );
+    const cover = data.items[0].volumeInfo.imageLinks.thumbnail;
+
+    document.getElementById('cover').setAttribute('src', cover);
+  }
+
+});
