4a63284397f84558f50079e45ed97fca872dc39c
JT <certainthings@gmail.com>
2019-08-09 13:00:34 +1000

    an introduction to react

---
+++ 12-react/react-intro/src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'; // destructuring
+
+class HelloWorld extends Component {
+  // Every React class component must have a method called render()
+  render() {
+    return (
+      <h1>Hello World!!!! I tolerate React</h1>
+    )
+  }
+}
+
+export default HelloWorld;
