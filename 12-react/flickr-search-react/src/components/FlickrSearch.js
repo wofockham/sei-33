e32b4a9e0573b93132af2b96f633750050561e67
JT <certainthings@gmail.com>
2019-08-12 12:40:09 +1000

    flickr image search in react

--- 12-react/flickr-search-react/src/components/FlickrSearch.js
+++ 12-react/flickr-search-react/src/components/FlickrSearch.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
+import jsonp from 'jsonp-es6';
+import _ from 'underscore';
 
 class FlickrSearch extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    };
+    this.fetchImages = this.fetchImages.bind(this);
+  }
+
+  fetchImages(q) {
+    console.log('Searching Flickr for', q);
+    const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?';
+    const flickrParams = {
+      method: 'flickr.photos.search',
+      api_key: '2f5ac274ecfac5a455f38745704ad084',
+      text: q,
+      format: 'json'
+    };
+
+    const generateURL = (p) => {
+      return [
+        'http://farm',
+        p.farm,
+        '.static.flickr.com/',
+        p.server,
+        '/',
+        p.id,
+        '_',
+        p.secret,
+        '_q.jpg' // Change the q to something else for different sizes -- see docs
+      ].join('');
+    };
+
+    jsonp(flickrURL, flickrParams, {callback: 'jsoncallback'}).then((results) => {
+      const images = _(results.photos.photo).map( generateURL );
+      this.setState({images: images});
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Image Search</h1>
-        <SearchForm />
-        <Gallery />
+        <SearchForm onSubmit={ this.fetchImages } />
+        <Gallery images={ this.state.images } />
       </div>
     );
   }
@@ -27,7 +67,7 @@ class SearchForm extends Component {
 
   _handleSubmit(event) {
     event.preventDefault();
-    console.log('about to search for', this.state.query);
+    this.props.onSubmit(this.state.query);
   }
 
   render() {
@@ -40,10 +80,15 @@ class SearchForm extends Component {
   }
 }
 
+// Fat arrow with implicit return;
+const Image = (props) => <img src={ props.url } width="150" height="150" alt="A piccy from Flickr" />;
+
 // <Gallery />: Functional component -- function expression component
 const Gallery = (props) => {
   return (
-    <h2>Gallery coming soon</h2>
+    <div>
+      { props.images.map( (url) => <Image url={url} key={url} /> ) }
+    </div>
   );
 }
 
