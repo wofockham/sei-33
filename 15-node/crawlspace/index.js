4d7cda7b4d73d928468b0735e39c6c179d228359
JT <certainthings@gmail.com>
2019-08-29 15:20:25 +1000

    node.js inifinite web crawler

---
+++ 15-node/crawlspace/index.js
@@ -0,0 +1,35 @@
+const Crawler = require('crawler');
+
+const isCrawlable = function (url) {
+  if (!url) { return false; }
+  if (!url.startsWith('http')) { return false; }
+
+  const terribleSites = ['archive.is', 'google.com', 'microsoft.com', 'facebook.com'];
+  return ! terribleSites.some( (awfulSite) => url.includes( awfulSite ) );
+};
+
+const c = new Crawler({
+  ratelimit: 1000,
+  callback: function (err, res, done) {
+
+    // Only travesrse HTML sites where Cheerio ($) is a function.
+    if (typeof res.$ === 'function') {
+      console.log( res.$('title').text() );
+      console.log( res.request.uri.href + '\n' );
+
+      const $links = res.$('a');
+
+      $links.each(function () {
+        const href = res.$(this).attr('href');
+
+        if (isCrawlable(href)) {
+          c.queue( href ); // Recursion.
+        }
+      });
+    }
+
+    done();
+  }
+});
+
+c.queue( process.argv[2] || 'http://lobste.rs' );
