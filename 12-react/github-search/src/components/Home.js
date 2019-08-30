7b4afb89454628e38426405491a989bf39f51137
JT <certainthings@gmail.com>
2019-08-30 15:15:48 +1000

    github search demonstrating loading... messages

--- 12-react/github-search/src/components/Home.js
+++ 12-react/github-search/src/components/Home.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default (props) => {
+
+  const _randomUser = () => {
+    const students = ['ajtauber', 'iwishjames', 'freddiebutcher', 'gopipatell', 'Herdmangct', 'harrisonmalouf', 'lucyod10', 'lpinzari', 'meowsoso', 'nilanarocha', 'pllenggg', 'Ratsamee', 'victorzw895', 'pkijowska'];
+
+    const randomIndex = Math.floor( Math.random() * students.length );
+    const username = students[ randomIndex ];
+
+    props.history.push(`/details/${ username }`);
+  };
+
   return (
     <div className="home">
       <h1>Github Search</h1>
       <Link to="/search">
         <button>Search for a user</button>
       </Link>
+      <button onClick={ _randomUser }>Random user</button>
     </div>
   );
 }
--- 12-react/github-search/src/index.js
+++ 12-react/github-search/src/index.js
@@ -6,12 +6,14 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Search from './components/Search';
+import Profile from './components/Profile';
 
 const Routes = (
   <Router>
     <div>
       <Route exact path="/" component={ Home } />
       <Route exact path="/search" component={ Search } />
+      <Route path="/details/:username" component={ Profile } />
     </div>
   </Router>
 );
