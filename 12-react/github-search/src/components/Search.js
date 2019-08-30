cd0c3fff7f2d7ef7b51df29da84df1ee7b936aff
JT <certainthings@gmail.com>
2019-08-30 12:56:17 +1000

    github search skellington

---
+++ 12-react/github-search/src/components/Search.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: ''
+    };
+    this._handleChange = this._handleChange.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleChange(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    this.props.history.push(`/details/${ this.state.username }`); // Navigate via react router.
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <h2>Search Github by username</h2>
+        <form onSubmit={ this._handleSubmit }>
+          <label>
+            Search:
+            <input type="search" onChange={ this._handleChange } value={ this.state.username } />
+          </label>
+          <button>Search for { this.state.username }</button>
+        </form>
+      </div>
+    );
+  }
+}
--- 12-react/github-search/src/index.js
+++ 12-react/github-search/src/index.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'preact/devtools';
 
-import App from './components/App';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App/>, document.body);
+import Home from './components/Home';
+import Search from './components/Search';
+
+const Routes = (
+  <Router>
+    <div>
+      <Route exact path="/" component={ Home } />
+      <Route exact path="/search" component={ Search } />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(Routes, document.body);
