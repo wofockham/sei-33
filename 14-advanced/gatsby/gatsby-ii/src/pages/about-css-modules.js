29242f92ae87884ae1464fdb416fa0e2bf8734cf
JT <certainthings@gmail.com>
2019-09-03 11:55:14 +1000

    css modules

--- 14-advanced/gatsby/gatsby-ii/src/pages/about-css-modules.js
+++ 14-advanced/gatsby/gatsby-ii/src/pages/about-css-modules.js
@@ -2,11 +2,28 @@ import React from 'react';
 import styles from "./about-css-modules.module.css";
 import Container from '../components/container';
 
-console.log( styles );
+const User = (props) => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="The user" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+);
+
 
 export default () => (
   <Container>
     <h1>About CSS modules</h1>
     <p>CSS modules are weird but people will insist you use them</p>
+    <User
+      username="Groucho"
+      avatar="http://www.fillmurray.com/96/96"
+      excerpt="No dogs no master" />
+      <User
+        username="Harpo"
+        avatar="http://www.fillmurray.com/98/98"
+        excerpt="" />
   </Container>
 );
