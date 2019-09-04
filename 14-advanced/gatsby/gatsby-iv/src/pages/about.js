0b44e8c1a46f458d71f526a447b8338228c3e799
JT <certainthings@gmail.com>
2019-09-04 10:53:19 +1000

    gatsby with emotion

---
+++ 14-advanced/gatsby/gatsby-iv/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/layout";
+export default () => (
+  <Layout>
+    <h1>About Bill Murray</h1>
+    <p>
+      Bill Murray is the acclaimed star of Ghostbusters and Lost in Translation.
+    </p>
+  </Layout>
+);
---
+++ 14-advanced/gatsby/gatsby-iv/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <h1>Welcome To Bill</h1>
+    <div>
+      <img src="http://www.fillmurray.com/800/600" alt="Bill Murray's Head" />
+    </div>
+  </Layout>
+);
