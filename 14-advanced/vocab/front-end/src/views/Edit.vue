d3c2d0eed39cc2af093b4c208f14f7c552c61139
JT <certainthings@gmail.com>
2019-09-05 16:38:32 +1000

    vocab edit update

---
+++ 14-advanced/vocab/front-end/src/views/Edit.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Edit Word</h1>
+    <word-form :word=this.word @createOrUpdate="createOrUpdate"></word-form>
+  </div>
+</template>
+
+<script>
+import WordForm from '../components/WordForm.vue';
+import { api } from '../helpers/helpers';
+
+export default {
+  name: 'edit',
+  components: {
+    'word-form': WordForm
+  },
+  data: function () {
+    return {
+      word: {}
+    }
+  },
+  methods: {
+    createOrUpdate: async function (word) {
+      console.log('createOrUpdate');
+      await api.updateWord(word);
+      alert('Word updated successfully!');
+      this.$router.push(`/words/${word._id}`);
+    }
+  },
+  async mounted() {
+    this.word = await api.getWord(this.$route.params.id);
+  }
+}
+</script>
