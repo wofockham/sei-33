665290b456450792b9b78d664595ddd0193c1483
JT <certainthings@gmail.com>
2019-09-05 12:15:59 +1000

    vocab server

--- 14-advanced/vocab/server/server.js
+++ 14-advanced/vocab/server/server.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+global.Vocab = require('./api/models/vocabModel');
+const routes = require('./api/routes/vocabRoutes');
+
+mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
+
+// WARNING: DO NOT COMMIT YOUR PASSWORD TO GITHUB
+mongoose.connect(
+  'mongodb+srv://the_blade:<PASSWORD>@cluster0-ivsrl.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true }
+);
+
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
 app.listen(port);
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+app.use((req, res) => {
+  res.status(404).send({ url: `${ req.originalUrl } not found` });
 });
 
 console.log(`Server start on port http://localhost:${ port }`);
